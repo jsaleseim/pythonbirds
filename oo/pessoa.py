31187b056940c71543b0c01e281f1c079308faa7
jsaleseim <roqcam@icloud.com>
2020-02-28 14:13:50 -0300

    criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=49):
+    def __init__(self, nome=None, *filhos,  idade=None, peso=None, sexo=None):
+        self.sexo = sexo
+        self.peso = peso
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola!!!{id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Pedro')
+    josue = Pessoa(nome ='Josue')
+    jose = Pessoa(josue, nome='Jose')
+
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
     print(p.nome)
-    p.nome = 'Josue'
-    print(p.nome)
-    print(p.idade)
+
