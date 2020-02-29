3dbe7851ad132e7e229f9f95da85a73bb9352b17
jsaleseim <roqcam@icloud.com>
2020-02-29 10:53:45 -0300

    criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None,  idade=None, peso=None, sexo=None):
         self.sexo = sexo
         self.peso = peso
@@ -22,9 +23,15 @@ if __name__ == '__main__':
     for filho in jose.filhos:
         print(filho.nome)
     jose.sobrenome = 'Silva'
-
+    jose.olhos = 1
     print(jose.__dict__)
     print(josue.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(jose.olhos)
+    print(josue.olhos)
+    print(id(Pessoa.olhos), id(josue.olhos), id(jose.olhos))
+
 
 
 
